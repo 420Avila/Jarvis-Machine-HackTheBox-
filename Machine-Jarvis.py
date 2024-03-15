d70360ee25f0ebb07a22af384b0abf9360dec2ce
Oswaldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2024-03-15 03:30:49 -0600

    Update Machine-Jarvis prueva.py

--- Machine-Jarvis.py
+++ Machine-Jarvis.py
@@ -1,6 +1,22 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 
 import requests 
 import pdb # Debugging 
 import sys 
-import 
+
+import signal
+import time 
+import threading
+
+from pwd import *
+
+def def_
+
+
+#Crlt+C
+def makeRequest( ):
+
+if __name__ == '__main__':
+
+    makeRequest( )
+    
